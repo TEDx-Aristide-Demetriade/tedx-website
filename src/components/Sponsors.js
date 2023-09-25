a57acc42f2d0885fc52da30d3d15991a92c24e57
madus <madalin.mihai.udrea@gmail.com>
2023-09-25 21:33:27 +0300

    changes/updates

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import image1 from './resources/logoCompanii/Flex Logo, Blue.png';  
+import image5 from './resources/logoCompanii/Logo_AutoSchunn_page-0001.jpg';
 import image2 from './resources/logoCompanii/Webamboos_Logomark_Color.png';  
 import image3 from './resources/logoCompanii/Network3.0.png';
-import image4 from './resources/logoCompanii/renderistic.jpeg'
+import image4 from './resources/logoCompanii/DecoShift(0)_page-0001.jpg';
+
+
 
 function Sponsors() {
   return (
@@ -18,17 +21,24 @@ function Sponsors() {
             <img src={image1} alt="Sponsor" className="img-fluid mb-3" />
           </div>
         </div>
-        <div style={{ height: '100px' }}></div>
+        
+        <div className="row justify-content-center">
+          <div className="col-md-2 text-center">
+            <img src={image5} alt="Sponsor" className="img-fluid mb-3" />
+          </div>
+        </div>
         <div className="row justify-content-center">
           <div className="col-md-2 text-center">
             <img src={image2} alt="Sponsor" className="img-fluid mb-3" />
           </div>
+         
         </div>
         <div style={{ height: '100px' }}></div>
         <div className="row justify-content-center">
           <div className="col-md-2 text-center">
             <img src={image3} alt="Sponsor" className="img-fluid mb-3" />
           </div>
+         
         </div>
         <div style={{ height: '100px' }}></div>
         <div className="row justify-content-center">
@@ -37,10 +47,10 @@ function Sponsors() {
            
           </div>
         </div>
+       
         {/* ... Add more rows and images as needed */}
       </div>
-      
-      <div style={{ height: '100px' }}></div>
+     
     </div>
   );
 }
